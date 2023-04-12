ad8c6a79397f317ac88428684827c57595788f97
danger_videograph <nikolastartsev@gmail.com>
2023-04-12 12:22:26 +0300

    commit

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def handle_arhive(filename: Path, target_folder: Path) -> None:
     target_folder.mkdir(exist_ok=True, parents=True)
     folder_for_file = target_folder / \
         normalize(filename.name.replace(filename.suffix, ''))
+    print(folder_for_file)
     folder_for_file.mkdir(exist_ok=True, parents=True)
     try:
         shutil.unpack_archive(str(filename.resolve()),
@@ -31,7 +32,6 @@ def handle_folder(folder: Path) -> None:
         print(f'Sorry, we can not delete the folder: {folder}')
 
 
-
 def main(folder: Path) -> None:
     parser.scan(folder)
     for file in parser.JPEG_IMAGES:
@@ -42,7 +42,7 @@ def main(folder: Path) -> None:
         handle_media(file, folder / 'images' / 'PNG')
     for file in parser.SVG_IMAGES:
         handle_media(file, folder / 'images' / 'SVG')
-        
+
     for file in parser.MP4_VIDEOS:
         handle_media(file, folder / 'videos' / 'MP4')
     for file in parser.AVI_VIDEOS:
@@ -51,7 +51,7 @@ def main(folder: Path) -> None:
         handle_media(file, folder / 'videos' / 'MOV')
     for file in parser.MKV_VIDEOS:
         handle_media(file, folder / 'videos' / 'MKV')
-        
+
     for file in parser.DOC_DOCUMENTS:
         handle_media(file, folder / 'documents' / 'DOC')
     for file in parser.DOCX_DOCUMENTS:
@@ -64,7 +64,7 @@ def main(folder: Path) -> None:
         handle_media(file, folder / 'documents' / 'XLSX')
     for file in parser.PPTX_DOCUMENTS:
         handle_media(file, folder / 'documents' / 'PPTX')
-        
+
     for file in parser.MP3_AUDIO:
         handle_media(file, folder / 'audio' / 'MP3')
     for file in parser.OGG_AUDIO:
@@ -73,14 +73,14 @@ def main(folder: Path) -> None:
         handle_media(file, folder / 'audio' / 'WAV')
     for file in parser.AMR_AUDIO:
         handle_media(file, folder / 'audio' / 'AMR')
-        
+
     for file in parser.ZIP_ARCHIVES:
         handle_arhive(file, folder / 'archives' / 'ZIP')
     for file in parser.GZ_ARCHIVES:
         handle_arhive(file, folder / 'archives' / 'GZ')
     for file in parser.TAR_ARCHIVES:
         handle_arhive(file, folder / 'archives' / 'TAR')
-    
+
     for file in parser.MY_OTHER:
         handle_media(file, folder / 'MY_OTHER')
     for folder in parser.FOLDERS[::-1]:
